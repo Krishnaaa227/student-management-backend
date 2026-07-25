57c3dfd0432215d4c5cc9841539a37ac949e35fb
Krishna Sheladiya <ksheladiya9321@gmail.com>
2026-07-25 21:49:05 +0530

    Update CORS for Vercel

--- src/main/java/student_management_backend/security/SecurityConfig.java
+++ src/main/java/student_management_backend/security/SecurityConfig.java
@@ -77,7 +77,10 @@ public CorsConfigurationSource corsConfigurationSource() {
 
     CorsConfiguration configuration = new CorsConfiguration();
 
-    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+    configuration.setAllowedOrigins(List.of(
+    "http://localhost:5173",
+    "https://student-management-frontend-onm096522-krishnaaa227s-projects.vercel.app/"
+));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
