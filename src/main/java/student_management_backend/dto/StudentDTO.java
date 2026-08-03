62a30078377446b842067833f39cb25d507b5cdc
Krishna Sheladiya <ksheladiya9321@gmail.com>
2026-08-03 20:22:11 +0530

    update

--- src/main/java/student_management_backend/dto/StudentDTO.java
+++ src/main/java/student_management_backend/dto/StudentDTO.java
@@ -1,35 +1,35 @@
 package student_management_backend.dto;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+
 import lombok.Data;
+import jakarta.validation.constraints.*;
 
 @Data
 public class StudentDTO {
-@NotBlank(message = "Roll Number is required")
-private String rollNo;
 
-@NotBlank(message = "First Name is required")
-private String firstName;
+    private Long id;   // <-- ADD THIS
 
-@NotBlank(message = "Last Name is required")
-private String lastName;
+    @NotBlank(message = "Roll Number is required")
+    private String rollNo;
 
-@Email(message = "Please enter a valid email")
-@NotBlank(message = "Email is required")
-private String email;
+    @NotBlank(message = "First Name is required")
+    private String firstName;
 
-@Pattern(
-    regexp = "^[0-9]{10}$",
-    message = "Phone number must contain exactly 10 digits"
-)
-private String phone;
+    @NotBlank(message = "Last Name is required")
+    private String lastName;
 
-@NotBlank(message = "Course is required")
-private String course;
+    @Email(message = "Please enter a valid email")
+    @NotBlank(message = "Email is required")
+    private String email;
 
-@NotNull(message = "Semester is required")
-private Integer semester;
-    
+    @Pattern(
+        regexp = "^[0-9]{10}$",
+        message = "Phone number must contain exactly 10 digits"
+    )
+    private String phone;
+
+    @NotBlank(message = "Course is required")
+    private String course;
+
+    @NotNull(message = "Semester is required")
+    private Integer semester;
 }
